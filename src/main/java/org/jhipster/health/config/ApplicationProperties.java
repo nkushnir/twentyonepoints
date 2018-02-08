28d67a4ec40b45ad053ae25c35de3d8b91ec9c67
nkushnir <nazar.kushnir@gmail.com>
2018-02-08 07:52:35 +0200

    Initial application generated by JHipster-4.14.0

---
+++ src/main/java/org/jhipster/health/config/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package org.jhipster.health.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Properties specific to Twentyonepoints.
+ * <p>
+ * Properties are configured in the application.yml file.
+ * See {@link io.github.jhipster.config.JHipsterProperties} for a good example.
+ */
+@ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
+public class ApplicationProperties {
+
+}
